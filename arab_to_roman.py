f8f9da81691d5656b318e68c6c6593cd6d59062f
Alex Valenzuela <38996897+advr0106@users.noreply.github.com>
2024-02-14 18:06:24 -0400

    Update arab_to_roman.py

--- arab_to_roman.py
+++ arab_to_roman.py
@@ -1,4 +1,22 @@
 def convert_to_roman(number):
-    return null    
+    if not is_number(number):
+        return "You must introduce a positive number"
+    
+    values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    romans = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+    result = ''
+    value = int(number)
+    
+    if value >= 5000:
+        return "Number too large. Please enter a number equal to or less than 5000."
+    else:
+        for i in range(len(values)):
+            while value >= values[i]:
+                result += romans[i]
+                value -= values[i]
+    return result  
 def is_valid_input(number):
-    return null
+    if number.isdecimal():
+        return True
+    else:
+        return False
